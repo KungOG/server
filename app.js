4f99eeea99c68cc59715e29caeb5f0aac83f9ac1
Oscar Gustafsson <oscar.gustafsson2@medieinstitutet.se>
2019-11-01 10:47:39 +0100

    Lint fix.

--- app.js
+++ app.js
@@ -9,29 +9,29 @@ app.use(cors());
 
 // Connect to our DB 	
 mongoose.connect(`mongodb+srv://FreddieBlue:xHUfwW4qQo083YAh@cluster0-hfptd.mongodb.net/thai-corner`, { useNewUrlParser: true })	
-    .then(() => {	
-        console.info('Connected.')	
-    })	
-    .catch(err => {	
-        console.error(err)	
-    });
+  .then(() => {	
+    console.info('Connected.')	
+  })	
+  .catch(err => {	
+    console.error(err)	
+  });
 
 // Routes
 let  = require('./routes/');
 let  = require('./routes/');
 
 app.route('/products/:id')
-    .delete(products.delete);
+  .delete(products.delete);
 
 app.route('/products')
-    .post(products.post)
-    .get(products.get);
+  .post(products.post)
+  .get(products.get);
 
 app.route('/orders')
-    .post(orders.post)
-    .get(orders.get);
+  .post(orders.post)
+  .get(orders.get);
 
 
 app.listen(3000, () => {
-    console.info('Server is running: 3000.')
+  console.info('Server is running: 3000.')
 });
