ad678b0da06709e483e722c5be4837014e245d6d
Oscar Gustafsson <oscar.gustafsson2@medieinstitutet.se>
2019-11-01 10:45:31 +0100

    Added some routes and requirements

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const cors = require('cors');
 let app = express();
 app.use(express.json());
 app.use(cors());
+
 // Connect to our DB 	
-mongoose.connect(`mongodb+srv://FreddieBlue:xHUfwW4qQo083YAh@cluster0-hfptd.mongodb.net/test`, { useNewUrlParser: true })	
+mongoose.connect(`mongodb+srv://FreddieBlue:xHUfwW4qQo083YAh@cluster0-hfptd.mongodb.net/thai-corner`, { useNewUrlParser: true })	
     .then(() => {	
         console.info('Connected.')	
     })	
@@ -18,6 +19,17 @@ mongoose.connect(`mongodb+srv://FreddieBlue:xHUfwW4qQo083YAh@cluster0-hfptd.mong
 // Routes
 let  = require('./routes/');
 let  = require('./routes/');
+
+app.route('/products/:id')
+    .delete(products.delete);
+
+app.route('/products')
+    .post(products.post)
+    .get(products.get);
+
+app.route('/orders')
+    .post(orders.post)
+    .get(orders.get);
 
 
 app.listen(3000, () => {
