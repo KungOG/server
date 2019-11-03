0efec6641f11abe63c6044538849fb53a4a9d15e
Oscar Gustafsson <oscar-gustafsson@live.com>
2019-11-03 19:59:08 +0100

    Added Patch function for our products

--- routes/products.js
+++ routes/products.js
@@ -9,6 +9,23 @@ module.exports.get = async(req, res) => {
   }
 };
 
+module.exports.patch = async (req, res) => {
+  try {
+    res.status(200).send(await ourProducts.findOneAndUpdate({ _id : req.body.ourProducts._id },
+    {
+      productNr: req.body.ourProducts.productNr,
+      productName : req.body.ourProducts.productName,
+      price: req.body.ourProducts.price,
+      /* picture: req.body.ourProducts.picture, */
+      category: req.body.ourProducts.category,
+      description: req.body.ourProducts.description,
+      protein: req.body.ourProducts.protein,
+      spice: req.body.ourProducts.spice,
+      ingredients: req.body.ourProducts.ingredients,
+    }))
+  } catch {
+  res.status(404).send(err.stack);
+}
 module.exports.post = async(req, res) => {
   try {
     let oneProduct = req.body;
