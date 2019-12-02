8f69d3db32203c6e48bcb3c6c788833dbc120f1c
Oscar Gustafsson <oscar.gustafsson2@medieinstitutet.se>
2019-12-02 14:34:19 +0100

    added productionTime for deployment

--- models/productionTime.js
+++ models/productionTime.js
@@ -1,9 +1,9 @@
 let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
-let businessHourSchema = new Schema({
+let productionTimeSchema = new Schema({
   productionTime: String,
 });
 
-let productionTime = mongoose.model('productionTime', businessHourSchema);
+let productionTime = mongoose.model('productionTime', productionTimeSchema);
 module.exports = productionTime;
