9b3cdc3ef8fbff07cedc96dd62f019e2aeb0108e
Oscar Gustafsson <oscar.gustafsson2@medieinstitutet.se>
2019-12-03 11:39:31 +0100

    orders

--- routes/orders.js
+++ routes/orders.js
@@ -2,7 +2,7 @@ const ourOrders = require('../models/order');
 
 module.exports.get = async(req, res) => {
   try {
-    let items = await ourOrders.find({});
+    let items = await ourOrders.find({time: {"$regex: "2019""}});
     res.status(200).send(items);
   } catch (err) {
     res.status(500).send(err);
