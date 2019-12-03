6448b963a5e31d987ddf155798ab3a6f2cd2296b
Oscar Gustafsson <oscar.gustafsson2@medieinstitutet.se>
2019-12-03 12:50:37 +0100

    date

--- models/order.js
+++ models/order.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 let Schema = mongoose.Schema;
+const year = new Date().getFullYear();
+const month = new Date().getMonth() + 1;
+const day = new Date().getDate();
 
 let ourOrderSchema = new Schema({
   orderInformation: Object,
+  date: new Date(year, month, day),
   code: String
 })
 
