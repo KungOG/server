68371038235674ad482b65d427e3d971135d6906
Oscar Gustafsson <oscar.gustafsson2@medieinstitutet.se>
2019-12-09 16:02:07 +0100

    started on new order layout

--- models/order.js
+++ models/order.js
@@ -2,7 +2,31 @@ const mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
 let ourOrderSchema = new Schema({
-  orderInformation: Object,
+  orderInformation: {
+    table: String,
+    foodItems: [{
+      productNr: Number,
+      productName: String,
+      /* picture: String, */
+      category: Number,
+      price: Number,
+      active: Boolean,
+      status: Number,
+      description: String,
+      protein: Array,
+      spice: Boolean,
+      ingredients: Array,
+      extras: Array,
+    }],
+    drinkItems: [{
+      productNr: Number,
+      productName: String,
+      /* picture: String, */
+      category: Number,
+      price: Number,
+      active: Boolean,
+    }],
+  },
   status: Number,
   date: {type: Date, default: Date.now},
   code: String
