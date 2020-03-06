61bfd8ef2637b8ce19af09122a1219991c1e471f
sandraottersten <otterstensandra@gmail.com>
2020-03-06 21:11:28 +0100

    test fixing cors issue

--- app.js
+++ app.js
@@ -14,7 +14,12 @@ const authConfig = {
   audience: "https://so-i-eat-server.herokuapp.com/"
 };
 
-app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 //app.use(express.static(process.env.STATIC_DIR));
 app.use(
   express.json({
